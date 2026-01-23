f56b6d0cfc1099d683e10fef3b96a1c070672ce6
Thiago Rocha <thiago8rocha@gmail.com>
2026-01-23 11:32:51 -0300

    mudando o titulo do cenário

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('web app deve estar online', async ({ page }) => {
+test('a web app deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   await expect(page).toHaveTitle(/Velô by Papito/)
