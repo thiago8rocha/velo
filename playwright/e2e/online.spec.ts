c92903659636a8298351cecd8ef88e87193aee32
Thiago Rocha <thiago8rocha@gmail.com>
2026-01-23 21:32:14 -0300

    Criação do primeiro teste e2e

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('a web app deve estar online', async ({ page }) => {
+test('web app deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   await expect(page).toHaveTitle(/Velô by Papito/)
