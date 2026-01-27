b4933c6229bb0da6b46c0dd0f4c8969ab3c89f83
Thiago Rocha <thiago8rocha@gmail.com>
2026-01-27 10:23:44 -0300

    Mudanças das novas aulas do curso e resultado do desafio

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,18 +4,18 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByTestId('search-order-id').click()
-  await page.getByTestId('search-order-id').fill('VLO-PGDLY8') 
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-PGDLY8')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-PGDLY8');
+  await expect(page.getByText('Pedido', { exact: true })).toBeVisible()
+  await expect(page.getByText('VLO-PGDLY8', { exact: true })).toBeVisible()
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await expect(page.getByText('APROVADO')).toBeVisible()
+  await expect(page.getByTestId(`order-result-VLO-PGDLY8`)).toBeVisible()
 })
